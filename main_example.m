bf7cbca25720ec13f2ddb081a0a581583a5c445d
Milka Wilkowiecka <66326311+MissWilka666@users.noreply.github.com>
2025-04-04 19:48:48 +0200

    Add files via upload

---
+++ main_example.m
@@ -0,0 +1,293 @@
+% Code showing the exemplary process of generating training data, training
+% UTIRnet and reconstructing holograms with UTIRnet without twin-image
+% 
+% Code is divided into sections, each one designed to present a separate 
+% part of the UTIRnet creation and use process. Run this sections
+% separately, as some of them may last a long time (expecially network
+% training part). "##########" - this sign indicates that the marked line
+% may be (or should be) changed to make the network work for an individual
+% user.
+% 
+% Cite as:   
+%   M. Rogalski, P. Arcab, L. Stanaszek, V. Micó, C. Zuo and M. Trusiak, 
+%   "Physics-driven universal twin-image removal network for digital 
+%   in-line holographic microscopy". Submitted 2023 
+% 
+% Created by:
+%   Mikołaj Rogalski,
+%   mikolaj.rogalski.dokt@pw.edu.pl
+%   Institute of Micromechanics and Photonics,
+%   Warsaw University of Technology, 02-525 Warsaw, Poland
+%
+% Last modified: 01.06.2023
+
+%% Dataset generation
+clear; close all; clc
+
+% System parameters ##########################
+% Z = 2600; % camera-sample distance (um)  ###########################
+% Z = 17000; % ###################
+lambda = 0.405; % light source wavelength (um) ###############
+dx = 2.4; % pixel size in object plane (cam_pix_size / mag) (um) ##########
+Z=2680; %Wilkowiecka_Z for USAFamp
+% Path to the directory containing flower recognition dataset (it should
+% contain 5 subdirectories with images of different flowers).
+% Can be downloaded at: 
+% https://www.kaggle.com/datasets/alxmamaev/flowers-recognition
+% Alternatively images from other repositories may be applied
+pth = 'C:\Users\110_F\Desktop\Wilkowiecka_Miloslawa\Holos\archive\flowers'; % ###############
+M=[5]; %number of grayscale colors 
+% Training dataset
+[inTrAmp,tarTrAmp,holosTrAmp] = GenerateDataset(pth,[1,4,5],650,Z,...
+    lambda,dx,'amp',M); % for CNN_A
+[inTrPhs,tarTrPhs,holosTrPhs] = GenerateDataset(pth,[1,4,5],650,Z,...
+    lambda,dx,'phs',M); % for CNN_P
+%% 
+
+% Validation dataset
+[inValAmp,tarValAmp,holosValAmp] = GenerateDataset(pth,[2,3],50,Z,...
+    lambda,dx,'amp',M); % for CNN_A
+[inValPhs,tarValPhs,holosValPhs] = GenerateDataset(pth,[2,3],50,Z,...
+    lambda,dx,'phs',M); % for CNN_P
+
+% Optionally you can save created datasets for later use ###############
+%% Network training
+% Generate dataset as in "Dataset generation" section or load previously 
+% generated dataset ################################
+
+% Load network architecture (for 512x512 px image size)
+load('NetworkArchitecture.mat')
+
+% Number of input images
+imgN = size(inTrAmp,4); 
+
+% Train CNN_A
+CNN_A_info = trainingOptions('adam', ...
+    'MiniBatchSize',1, ...
+    'MaxEpochs',30,...  
+    'LearnRateDropPeriod',5,... 
+    'LearnRateDropFactor',0.5,...
+    'LearnRateSchedule','piecewise',...
+    'InitialLearnRate',1e-4, ... 
+    'Shuffle','every-epoch',...
+    'Plots','training-progress',...
+    'ValidationData',{(inValAmp), (tarValAmp)},...
+    'ValidationFrequency', imgN);
+
+[CNN_A,info_A] = trainNetwork(inTrAmp, tarTrAmp, lgraph, CNN_A_info);
+CNN_A_info.ValidationData = [];
+
+
+% Train CNN_P
+CNN_P_info = trainingOptions('adam', ...
+    'MiniBatchSize',1, ...
+    'MaxEpochs',30,...  
+    'LearnRateDropPeriod',5,... 
+    'LearnRateDropFactor',0.5,...
+    'LearnRateSchedule','piecewise',...
+    'InitialLearnRate',1e-4, ... 
+    'Shuffle','every-epoch',...
+    'Plots','training-progress',...
+    'ValidationData',{(inValPhs), (tarValPhs)},...
+    'ValidationFrequency', imgN);
+
+%[CNN_P,info_P] = trainNetwork(inTrPhs, tarTrPhs, lgraph, CNN_P_info);
+CNN_P=[];
+info_P=[];
+CNN_P_info.ValidationData = [];
+%% 
+
+% System parameters
+UTIRnet_info.Z_mm = Z/1000;
+UTIRnet_info.lambda_um = lambda;
+UTIRnet_info.dx_um = dx;
+%% 
+
+% Save network
+fnm = ['UTIRnet_my_Z-',num2str(Z/1000),'mm_dx-',num2str(dx),'um_lambda-',...
+    num2str(lambda*1000),'nm_M-',num2str(M),'.mat'];
+save(fnm,'CNN_A','CNN_P','CNN_A_info','CNN_P_info','UTIRnet_info')
+
+%% Network testing - on validation data
+% Train network as in "Network training" section or load previously 
+% trained network ################################
+% Generate validation dataset as in "Dataset generation" section or load 
+% previously generated dataset ################################
+
+% System parameters (ensure that are the same as used for network training)
+% Z = 2600; % camera-sample distance (um)  ###########################
+% Z = 17000; % ###################
+lambda = 0.405; % light source wavelength (um) ###############
+dx = 2.4; % pixel size in object plane (cam_pix_size / mag) (um) ##########
+
+Z = 2680;%Wilkowiecka_Z for USAFamp
+
+imNo = 50; % image number ################################
+AmpPhs = 1; % 1 - amplitude data, 2 - phase data ##########################
+
+if AmpPhs == 1
+    % Amp data
+    GT = tarValAmp(:,:,imNo); % ground truth target image
+    holo = holosValAmp(:,:,imNo); % hologram
+elseif AmpPhs == 2
+    % Phs data
+    GT = tarValPhs(:,:,imNo); % ground truth target image
+    holo = holosValPhs(:,:,imNo); % hologram
+end
+
+ps = 256; % pad size
+holoP = padarray(holo,[ps,ps],'replicate'); % hologram padding
+
+% reconstruction
+[Yout,Yamp,Yphs,Uout] = UTIRnetReconstruction(holoP,...
+    CNN_A,CNN_P,Z,lambda,dx,[],0);
+% remove padding
+Yout = Yout(ps+1:end-ps,ps+1:end-ps);
+Uout = Uout(ps+1:end-ps,ps+1:end-ps);
+Yamp = Yamp(ps+1:end-ps,ps+1:end-ps);
+Yphs = Yphs(ps+1:end-ps,ps+1:end-ps);
+
+% display results
+close all
+ax = [];
+if AmpPhs == 1
+    rng = [0,1.1];
+    figure; imagesc(abs(Uout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('input AS amplitude (with twin-image)')
+    figure; imagesc(abs(Yout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('UTIRnet amplitude reconstruction')
+    figure; imagesc(GT,rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Ground truth amplitrude (without twin-image)') 
+elseif AmpPhs == 2
+    rng = [-pi,pi];
+    figure; imagesc(angle(Uout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('input AS phase (with twin-image)')
+    figure; imagesc(angle(Yout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('UTIRnet reconstruction')
+    figure; imagesc(GT-pi,rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Ground truth (without twin-image)')
+end
+linkaxes(ax)
+
+%% Network testing - generating and reconstructing synth data
+% Train network as in "Network training" section or load previously 
+% trained network ################################
+% System parameters (ensure that are the same as used for network training)
+% Z = 2600; % camera-sample distance (um)  ###########################
+% Z = 17000; % ###################
+lambda = 0.405; % light source wavelength (um) ###############
+dx = 2.4; % pixel size in object plane (cam_pix_size / mag) (um) ##########
+
+AmpPhs = 2; % 1 - amplitude data, 2 - phase data ##########################
+Z = 2680;%Wilkowiecka_Z for USAFamp
+
+% Read any image #################################### 
+% img = imread('cameraman.tif');
+% img = double(imread('coins.png'));
+img = double(imread('rice.png'));
+
+if AmpPhs == 1
+    [input,GT,holo] = GenerateHologram(img,Z,lambda,dx,'amp',M,1);
+elseif AmpPhs == 2
+    [input,GT,holo] = GenerateHologram(img,Z,lambda,dx,'phs',M,1);
+end
+
+ps = 256; % pad size
+holoP = padarray(holo,[ps,ps],'replicate'); % hologram padding
+
+% reconstruction
+[Yout,Yamp,Yphs,Uout] = UTIRnetReconstruction(holoP,...
+    CNN_A,CNN_P,Z,lambda,dx,[],0);
+% remove padding
+Yout = Yout(ps+1:end-ps,ps+1:end-ps);
+Uout = Uout(ps+1:end-ps,ps+1:end-ps);
+Yamp = Yamp(ps+1:end-ps,ps+1:end-ps);
+Yphs = Yphs(ps+1:end-ps,ps+1:end-ps);
+
+% display results
+close all
+ax = [];
+if AmpPhs == 1
+    rng = [0,1.1];
+    figure; imagesc(abs(Uout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('input AS amplitude (with twin-image)')
+    figure; imagesc(abs(Yout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('UTIRnet amplitude reconstruction')
+    figure; imagesc(GT,rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Ground truth amplitrude (without twin-image)') 
+elseif AmpPhs == 2
+    rng = [-pi,pi];
+    figure; imagesc(angle(Uout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('input AS phase (with twin-image)')
+    figure; imagesc(angle(Yout),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('UTIRnet reconstruction')
+    figure; imagesc(GT-pi,rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Ground truth (without twin-image)')
+end
+linkaxes(ax)
+%% Network testing - experimental data
+clear; close all; clc
+
+m1 = -1;
+
+% load hologram data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+load('.\Holos\USAF amp\LPB5\USAF-1.mat')
+% load('.\Holograms\CustomPhsTest_2.64mm.mat'); m1 = -1;
+% load('.\Holograms\GlialCells_15.14mm.mat')
+% load('.\Holograms\USAFamp_2.72mm.mat');
+% load('.\Holograms\USAFamp_17.09mm.mat');
+% load('.\Holograms\USAFampSynthetic_2.72mm.mat');
+% load('.\Holograms\USAFampSynthetic_17.09mm.mat');
+holo=double(u);
+[rows,cols]=size(holo);
+
+
+% load network trained for propper system parameters %%%%%%%%%%%%%%%%%%%%%%
+% % load('.\Networks\UTIRnet_Z-2.6mm_dx-2.4um_lambda-405nm.mat')
+% path='C:\Users\110_F\Desktop\Wilkowiecka_Miloslawa\Networks\UTIRnet_my_Z-2.68mm_dx-2.4um_lambda-405nm_M-5_plain.mat';
+% path=('C:\Users\110_F\Desktop\Wilkowiecka_Miloslawa\Networks\UTIRnet_my_Z-2.68mm_dx-2.4um_lambda-405nm_M-20_plain.mat')
+% path=('C:\Users\110_F\Desktop\Wilkowiecka_Miloslawa\Networks\UTIRnet_my_Z-2.68mm_dx-2.4um_lambda-405nm_M-5.mat')
+% path=('C:\Users\110_F\Desktop\Wilkowiecka_Miloslawa\Networks\UTIRnet_my_Z-2.68mm_dx-2.4um_lambda-405nm_M-20.mat')
+path=('C:\Users\110_F\Desktop\Wilkowiecka_Miloslawa\Networks\Epoch 10\UTIRnet_my_Z-2.68mm_dx-2.4um_lambda-405nm_M-20_5.mat')
+[~,name,ext]=fileparts(path);
+load(path);
+
+lambda = 0.405; % light source wavelength (um) ###############
+dx = 2.4; % pixel size in object plane (cam_pix_size / mag) (um) ##########
+AmpPhs = 2; % 1 - amplitude data, 2 - phase data ##########################
+Z = 2680;%Wilkowiecka_Z for USAFamp
+
+% reconstruction
+[Yout,Yamp,Yphs,Uout] = UTIRnetReconstruction(double(holo),...
+    CNN_A,CNN_P,Z,lambda,dx,m1,1);
+
+% displaying
+ax = [];
+rng = [min(abs(Uout(:))),max(abs(Uout(:)))];
+figure; imagesc(abs(Uout),rng); ax = [ax,gca]; colormap gray;
+axis image; colorbar; title('AS amplitude (with twin-image)')
+figure; imagesc(abs(Yamp),rng); ax = [ax,gca]; colormap gray;
+axis image; colorbar; title('UTIRnet amplitude reconstruction')
+if exist('GS','var')
+    figure; imagesc(abs(GS),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Reference GS amplitrude')
+    figure; imagesc(abs(GS_CFF),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Referenmce GS+CFF amplitrude')
+end
+if exist('GroundTruth','var')
+    figure; imagesc(GroundTruth,rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Ground truth amplitude')
+end
+rng = [-pi,pi];
+figure; imagesc(angle(Uout),rng); ax = [ax,gca]; colormap gray;
+axis image; colorbar; title('AS phase (with twin-image)')
+figure; imagesc(angle(Yout),rng); ax = [ax,gca]; colormap gray;
+axis image; colorbar; title('UTIRnet phase reconstruction')
+if exist('GS','var')
+    figure; imagesc(angle(GS),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Reference GS phase')
+    figure; imagesc(angle(GS_CFF),rng); ax = [ax,gca]; colormap gray;
+    axis image; colorbar; title('Reference GS+CFF phase')
+end
+
+linkaxes(ax)
--- process_images.m
+++ process_images.m
@@ -1,28 +1,25 @@
-function [img_result,img_noise]=process_images(img_org,M,n)
-    if isfile('tableM.mat')
-       load('tableM.mat','tableM');
-    else
-        disp('Lack of table with STD to use');
-    end
+function [img_result,img_noise]=process_images(img_org,M)
+if isfile('tableM.mat')
+    load('tableM.mat','tableM');
+else
+    disp('Lack of table with STD to use');
+end
 
-    img_result=cell(1,n);
-    img_noise=cell(1,n);
+mean_org=mean2(img_org);
+img_org=img_org/mean_org; %img_org to mean=1
+rowM=tableM(:,1)==M;
+valM=tableM(rowM,2);
 
-    mean_org=mean2(img_org);
-    img_org=img_org/mean_org; %img_org to mean=1
-    rowM=tableM(:,1)==M;
-    valM=tableM(rowM,2);
-    for ii=1:n
-        img_noise{ii}=randn(size(img_org))*valM; %szum na podstawie odchyleń standardowych
-        img_result{ii}=img_org+img_noise{ii}; %dodawanie szumu do obrazu
-        img_min=min(img_result{ii}(:));
-        img_max=max(img_result{ii}(:));
-        img_result{ii}=(img_result{ii}-img_min)/(img_max-img_min);
-        if M>1
-            img_result{ii}=round(img_result{ii}*(M-1))/(M-1);
-        else
-            img_result{ii}=round(img_result{ii}*(M))/(M);
-        end
-        
-    end
+img_noise=randn(size(img_org))*valM; %szum na podstawie odchyleń standardowych
+img_result=img_org+img_noise; %dodawanie szumu do obrazu
+% img_min=min(img_result(:));
+% img_max=max(img_result(:));
+% img_result=(img_result-img_min)/(img_max-img_min);
+% if M>1
+%     img_result=round(img_result*(M-1))/(M-1);
+% else
+%     img_result=round(img_result*(M))/(M);
+% end
+img_result=round(img_result*(M))/(M); % Dodawanie szumu kwantyfikacji
+img_result = img_result*mean_org; % Przywracanie sredniej do oryginalnej
 end
